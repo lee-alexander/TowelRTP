be5f9124b9eb33ec4b280af007be06db746320cf
Lee Alexander <ralexander31@gatech.edu>
2014-11-24 19:44:07 -0500

    Working threaded RTP implementation across a perfect network.

--- RTPSocket.py
+++ RTPSocket.py
@@ -1,5 +1,5 @@
 import socket
-from Queue import Queue
+from Queue import Queue, Empty
 from threading import Thread
 
 class RTPPacket:
@@ -31,88 +31,137 @@ def split_data(data, split_size):
     return [data[i:i+split_size] for i in range(0, len(data), split_size)]
 
 
-
 class RTPSocket(object):
-    MTU_SIZE = 10
+    MTU_SIZE = 1000
 
     def __init__(self, port):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.bind(('', port))
-        self.sock.settimeout(1)
-        self.other_addr = None
-        self.other_port = None
+        # Connection state variables
         self.connected = False
-        self.open = True
-        self.buffer = Queue()
-        self.background_thread = Thread(target=self._receive_packets)
-        self.background_thread.start()
 
-    # Wait for a client to accept (Blocking)
+        # Pipeline thread for updating send/receive buffers using UDP socket info
+        self.pipeline = RTPSocketPipeline(port, self)
+        self.pipeline.start()
+
+    # Wait for a client to accept (blocking)
     def accept(self):
         if self.connected:
             return
 
+        # Wait to receive a handshake from a client
         pkt = None
         while pkt is None or not pkt.is_handshake:
-            pkt = self.buffer.get()
+            pkt = self.pipeline.dequeue_packet()
 
-        self.other_addr = pkt.client_info[0]
-        self.other_port = pkt.client_info[1]
+        self.pipeline.update_client_info(pkt.client_info[0], pkt.client_info[1])
         self.connected = True
 
-    # Connect to the server (Blocking)
+    # Connect to the server (non-blocking)
     def connect(self, address, port):
         if self.connected:
             return
 
-        self.other_addr = address
-        self.other_port = port
-
-        self._send_packet(RTPPacket(is_handshake=True))
+        self.pipeline.update_client_info(address, port)
+        self.pipeline.enqueue_packet_to_send(RTPPacket(is_handshake=True))
         self.connected = True
 
-    # Close the RTP socket connection
+    # Close the RTP socket connection (non-blocking)
     def close(self, broadcast=True):
         if not self.connected:
             return
 
         self.connected = False
-        self.open = False
         if broadcast:
-            self._send_packet(RTPPacket(is_disconnect=True))
-        self.sock.close()
+            self.pipeline.enqueue_packet_to_send(RTPPacket(is_disconnect=True))
+        self.pipeline.stop()
 
-    # Send data to the other side (Blocking)
+    # Send data to the other side (non-blocking)
     def send(self, data):
         if not self.connected:
             return
 
-        chunks = split_data(data, RTPSocket.MTU_SIZE - RTPPacket.HEADER_SIZE)
+        for chunk in split_data(data, RTPSocket.MTU_SIZE - RTPPacket.HEADER_SIZE):
+            self.pipeline.enqueue_packet_to_send(RTPPacket(chunk))
 
-        for chunk in chunks:
-            self._send_packet(RTPPacket(chunk))
-
-    def _send_packet(self, packet):
-        self.sock.sendto(packet.serialize(), (self.other_addr, self.other_port))
-
-    # Receive data from the other side (Blocking if buffer is empty)
+    # Receive data from the other side (blocking)
     def receive(self):
-        return self.buffer.get().payload
+        if not self.connected:
+            return None
 
-    # This is running in the background for the duration of the session
-    def _receive_packets(self):
-        while self.open:
+        pkt = self.pipeline.dequeue_packet()
+        return None if pkt is None else pkt.payload
+
+
+
+class RTPSocketPipeline(object):
+    def __init__(self, port, rtp_socket):
+        self._send_packets = Queue() # input packets sent to the pipeline to transmit reliably to other side
+        self._receive_packets_staging = Queue() # possibly out of order, but correct contents
+        self._receive_packets = Queue() # in order and final
+        self.running = False
+        self.rtp_sock = rtp_socket
+
+        # Internal UDP Socket initialization
+        self.udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.udp_sock.bind(('', port))
+        self.udp_sock.settimeout(1)
+
+        self.other_addr = None
+        self.other_port = None
+
+    def update_client_info(self, other_addr, other_port):
+        self.other_addr = other_addr
+        self.other_port = other_port
+
+    def start(self):
+        self.running = True
+        self.receive_thread = Thread(target=self._run_receive)
+        self.receive_thread.start()
+        self.send_thread = Thread(target=self._run_send)
+        self.send_thread.start()
+
+    def stop(self):
+        self.running = False
+        self.receive_thread.join()
+        self.send_thread.join()
+        self.udp_sock.close()
+
+    def enqueue_packet_to_send(self, pkt):
+        self._send_packets.put(pkt)
+
+    def dequeue_packet(self):
+        while self.running:
             try:
-                data, addr = self.sock.recvfrom(RTPSocket.MTU_SIZE)
+                return self._receive_packets.get(timeout=1)
+            except Empty:
+                continue
+
+        return None
+
+    def _run_receive(self):
+        while self.running:
+            try:
+                data, addr = self.udp_sock.recvfrom(RTPSocket.MTU_SIZE)
+                #print('received: ' + str(data))
+                self._receive_packets_staging.put(RTPPacket.deserialize_and_create(data, addr))
+                self._unstage_packets()
             except socket.timeout:
                 continue
 
-            pkt = RTPPacket.deserialize_and_create(data, addr)
+    def _unstage_packets(self):
+        self._receive_packets.put(self._receive_packets_staging.get())
 
-            if pkt.is_disconnect:
-                self.close(broadcast=False)
+    def _run_send(self):
+        while self.running:
+            try:
+                pkt = self._send_packets.get(timeout=1)
+                self._send_packet(pkt)
+            except Empty:
+                continue
 
-            self.buffer.put(pkt)
+    def _send_packet(self, packet):
+        #print('sending packet: (' + packet.serialize() + ')')
+        self.udp_sock.sendto(packet.serialize(), (self.other_addr, self.other_port))
+
 
 
 class RTPSocket_Mock(RTPSocket):
