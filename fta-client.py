02004e82a191857086545f54a5bc1b2e04126efc
Lee Alexander <ralexander31@gatech.edu>
2014-11-28 14:32:37 -0500

    Some bug fixes

--- fta-client.py
+++ fta-client.py
@@ -17,15 +17,10 @@ import time
 from RTPSocket import RTPSocket
 from fta_util import decodeHeader, HEADER_SIZE, encodeFileHeader, printNetworkStats
 
-
 if len(sys.argv) != 4:
     print "Entered incorrect number of arguments"
     print "Please use following command: python remotecalc-tcp.py <client port> <NetEmu IP> <NetEmu Port>"
     sys.exit(0)
-
-servername = ""
-serverPort = ""
-
 
 #pull out arguments
 clientPort = int(sys.argv[1])
@@ -36,103 +31,116 @@ netEmuPort = sys.argv[3]
 clientSocket = RTPSocket(int(clientPort))
 
 response = ""
-fromAddress = ""
+connected = False
 try:
-    clientSocket.connect(netEmuIp, int(netEmuPort))
-    while 1:  #
+    while True:
         command = raw_input(">")
+
         if command == "connect":
-            print "TODO"
+            connected = clientSocket.connect(netEmuIp, int(netEmuPort))
+        elif not connected:
+            print "Must connect before performing any actions"
         elif command[:4] == "post":
             filename = command[5:]
-            try:
-                originalFilename = filename
-                if os.path.isfile(filename):
-                    start = time.time()
-                    progress = 0
-                    lastUpdate = time.time()
-                    infile = open(filename)  # readonly by default
-                    print "Posting file '", filename, "' to server..."
-                    clientSocket.send(encodeFileHeader(0, 1, filename) + infile.read())
-                    infile.close()
-                    done = False
-                    while not done:
-                        response = ''
-                        while len(response) < HEADER_SIZE:
-                            r = clientSocket.receive()
-                            if r is None:
-                                clientSocket.close()
-                                print "Server disconnected"
-                                sys.exit(0)
-                            response += r
-                        error, operation, filename, fileSize = decodeHeader(response)
-                        if operation == "2":
-                            remaining = fileSize + HEADER_SIZE - len(response)
-                            while remaining > 0:
-                                r = clientSocket.receive()
-                                if r is None:
-                                    clientSocket.close()
-                                    print "Server disconnected."
-                                    sys.exit(0)
-                                response += r
-                                remaining -= len(r)
-                            print response[HEADER_SIZE:]
-                        else:
-                            done = True
-                            print "File was uploaded successfully."
-                            totalTime = time.time() - start
-                            printNetworkStats(totalTime, (os.path.getsize(originalFilename) + HEADER_SIZE)/totalTime)
-                else:
-                    print "File not found. Please check the file name and try again."
-            except:
-                print "Command: '", command, "'failed."
-        elif command[:3] == "get":
-            filename = command[4:]
-            try:
+            originalFilename = filename
+
+            if os.path.isfile(filename):
                 start = time.time()
                 progress = 0
                 lastUpdate = time.time()
-                clientSocket.send(encodeFileHeader(0, 0, filename))
-                data = ""
-                while len(data) < HEADER_SIZE:
-                    r = clientSocket.receive()
-                    if r is None:
-                        clientSocket.close()
-                        print "Server disconnected"
-                        sys.exit(0)
-                    data += r
-                if data[0] == "1":  # error repeat
-                    print "File not found on server. Please check the file name and try again."
-                else:
-                    print "Downloading file '", filename, "' from server..."
-                    error, operation, filename, fileSize = decodeHeader(data)
-                    outfile = open(filename, "wr")
-                    remaining = fileSize + HEADER_SIZE - len(data)
-                    outfile.write(data[HEADER_SIZE:])
-                    tick = 0
-                    while remaining > 0:
-                        message = clientSocket.receive()
-                        if message is None:
+                infile = open(filename)
+                print "Posting file '", filename, "' to server..."
+
+                clientSocket.send(encodeFileHeader(0, 1, filename) + infile.read())
+                infile.close()
+                done = False
+                while not done:
+                    response = ''
+
+                    while len(response) < HEADER_SIZE:
+                        r = clientSocket.receive()
+
+                        if r is None:
                             clientSocket.close()
-                            outfile.close()
                             print "Server disconnected"
                             sys.exit(0)
-                        data += message
-                        outfile.write(message)
-                        remaining -= len(message)
-                        progress = len(data)/float((fileSize + HEADER_SIZE))
-                        tick = time.time()
-                        if tick - lastUpdate > .2:
-                            print str(int(progress * 100)) + "%"
-                            lastUpdate = time.time()
-                    outfile.close()
-                    end = time.time()
-                    print "File '" + filename + "' downloaded successfully."
-                    totalTime = end-start
-                    printNetworkStats(totalTime, len(data)/totalTime)
 
-            except:
-                print "Command: '", command, "'failed. Please check your file name and try again."
+                        response += r
+                    error, operation, filename, fileSize = decodeHeader(response)
+
+                    if operation == "2":
+                        remaining = fileSize + HEADER_SIZE - len(response)
+
+                        while remaining > 0:
+                            r = clientSocket.receive()
+
+                            if r is None:
+                                clientSocket.close()
+                                print "Server disconnected."
+                                sys.exit(0)
+
+                            response += r
+                            remaining -= len(r)
+                        print response[HEADER_SIZE:]
+                    else:
+                        done = True
+                        print "File was uploaded successfully."
+
+                        totalTime = time.time() - start
+                        printNetworkStats(totalTime, (os.path.getsize(originalFilename) + HEADER_SIZE)/totalTime)
+            else:
+                print "File not found. Please check the file name and try again."
+        elif command[:3] == "get":
+            filename = command[4:]
+            start = time.time()
+            progress = 0
+            lastUpdate = time.time()
+            clientSocket.send(encodeFileHeader(0, 0, filename))
+            data = ""
+
+            while len(data) < HEADER_SIZE:
+                r = clientSocket.receive()
+
+                if r is None:
+                    clientSocket.close()
+                    print "Server disconnected"
+                    sys.exit(0)
+
+                data += r
+            if data[0] == "1":  # error repeat
+                print "File not found on server. Please check the file name and try again."
+            else:
+                print "Downloading file '", filename, "' from server..."
+                error, operation, filename, fileSize = decodeHeader(data)
+                outfile = open(filename, "wr")
+                remaining = fileSize + HEADER_SIZE - len(data)
+                outfile.write(data[HEADER_SIZE:])
+                tick = 0
+
+                while remaining > 0:
+                    message = clientSocket.receive()
+
+                    if message is None:
+                        clientSocket.close()
+                        outfile.close()
+                        print "Server disconnected"
+                        sys.exit(0)
+
+                    data += message
+                    outfile.write(message)
+                    remaining -= len(message)
+                    progress = len(data)/float((fileSize + HEADER_SIZE))
+                    tick = time.time()
+
+                    if tick - lastUpdate > .2:
+                        print str(int(progress * 100)) + "%"
+                        lastUpdate = time.time()
+
+                outfile.close()
+                end = time.time()
+                print "File '" + filename + "' downloaded successfully."
+                totalTime = end-start
+                printNetworkStats(totalTime, len(data)/totalTime)
         elif command[:6] == "window":
             clientSocket.set_window_size(int(command[7:]))
             print "Set window size to " + command[7:]
@@ -147,7 +155,5 @@ try:
                   "post [file]:   Upload a file to the server.\n"\
                   "get [file]:    Try to retrieve a file from the server.\n"\
                   "disconnect:    Terminates any existing connections and stops the server.\n"
-except:
-    print "Application Exited"
 finally:
     clientSocket.close()
--- fta-server.py
+++ fta-server.py
@@ -18,11 +18,11 @@ import time
 from RTPSocket import RTPSocket
 from fta_util import decodeHeader, HEADER_SIZE, encodeFileHeader, encodeMessageHeader
 
-
 def listenForCommands(serverSocket):
-    while 1:
+    while True:
         command = raw_input(">")
         print "Command: " + command
+
         if command == "terminate":
             serverSocket.disconnect()
             os._exit(0)
@@ -35,7 +35,6 @@ def listenForCommands(serverSocket):
                   "window [int]:    Takes a integer between x and z which determines the windows size"
 
 FILE_READ_SIZE = 2048
-
 serverPort = int(sys.argv[1])
 emulatorIP = sys.argv[2]
 emulatorPort = int(sys.argv[3])
@@ -43,16 +42,15 @@ emulatorPort = int(sys.argv[3])
 #setup socket
 serverSocket = RTPSocket(serverPort)
 
-try:
-    thread.start_new_thread(listenForCommands, (serverSocket,))
-except:
-    print "Error: ", sys.exc_info()
+# Background thread to listen for commands
+thread.start_new_thread(listenForCommands, (serverSocket,))
 
 print "Server is up and listening\n"
 try:
-    while 1:  # start listening and never stop
+    while True:
         serverSocket.accept()
         data = ""
+
         while len(data) < HEADER_SIZE:
             r = serverSocket.receive()
             if r is None:
@@ -60,24 +58,30 @@ try:
                 print "Client disconnected"
                 sys.exit(0)
             data += r
+
         error, operation, filename, fileSize = decodeHeader(data)
-        if operation == "1":  # post
+        if operation == "1":
+            # Receiving a file from client (client is POSTing)
             progress = 0
             lastUpdate = time.time()
             outfile = open(filename, "wr")
             remaining = fileSize + HEADER_SIZE - len(data)
             outfile.write(data[HEADER_SIZE:])
+
             while remaining > 0:
                 message = serverSocket.receive()
+
                 if message is None:
                     print "Client disconnected"
                     serverSocket.close()
                     sys.exit(0)
+
                 data += message
                 outfile.write(message)
                 remaining -= len(message)
                 tick = time.time()
                 progress = len(data)/float((fileSize + HEADER_SIZE))
+
                 if tick - lastUpdate > .2:
                     updateMessage = str(int(progress * 100)) + "%"
                     serverSocket.send(encodeMessageHeader(0, "2", len(updateMessage)) + updateMessage)
@@ -85,14 +89,13 @@ try:
             outfile.close()
             response = ""
             serverSocket.send(encodeMessageHeader(0, "0", len(response)) + response)
-        else:  # get
+        else:
+            # Client is requesting a file (client is GETing)
             if os.path.isfile(filename):
-                infile = open("server-" + filename)
+                infile = open(filename)
                 serverSocket.send(encodeFileHeader(0, 1, filename) + infile.read())
                 infile.close()
             else:
                 serverSocket.send(encodeFileHeader(1, operation, "") + "")
-except:
-    print "Application Exited"
 finally:
     serverSocket.close()
